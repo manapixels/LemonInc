6f91ae3a8bf18044a36859f11c5abee00a2a5276
chngws <weisheng_chng@mymail.sutd.edu.sg>
2016-03-25 12:15:19 +0800

    First commit

---
+++ core/src/com/mygdx/taptap3/TapTap3.java
@@ -0,0 +1,25 @@
+package com.mygdx.taptap3;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.taptap3.Screens.PlayScreen;
+
+public class TapTap3 extends Game {
+	public static final int V_HEIGHT = 480;
+	public static final int V_WIDTH = 800;
+	public static final float PPM = 100f;
+	public SpriteBatch batch;
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		setScreen(new PlayScreen(this));
+	}
+
+	@Override
+	public void render () {
+		super.render();
+	}
+}
+
+//TODO double jump, upper bounds, fill screen with background, HUD
