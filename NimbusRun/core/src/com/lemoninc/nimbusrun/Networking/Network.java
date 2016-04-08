e4e1e55bf50b1f3c48c2f93f26d9e07a64b7cd6d
zhenyangg <zhenyang_rain@yahoo.com.sg>
2016-04-08 10:16:44 +0800

    implemented animations using PlayScreen and Player

--- NimbusRun/core/src/com/lemoninc/nimbusrun/Networking/Network.java
+++ NimbusRun/core/src/com/lemoninc/nimbusrun/Networking/Network.java
@@ -40,8 +40,5 @@ public class Network {
             this.name = name;
         }
     }
-<<<<<<< HEAD:NimbusRun/core/src/com/lemoninc/nimbusrun/Networking/Network.java
+
 }
-=======
-}
->>>>>>> 2d55319255c217cea398b8e12d2634ec15197989:TapTap3/core/src/com/mygdx/taptap3/Networking/Network.java
